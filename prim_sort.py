df72d2fe953c5c3994f899da82cf4b9034d77c22
Jens-Konrad Preem <jenspreem@gmail.com>
2016-06-02 14:37:20 +0300

    Replace hardcoded primers with primers loadable from oligo file
    The prim_sort.py made to require an oligofile as the second argument.
    Oligofile format:
    name tab forward tab reverse
    
    Code changed to use containters and loops instead of fixed
    amount of named varaibles -- as usage of oligofile implies arbitrary amount
    of primer sequences

--- prim_sort.py
+++ prim_sort.py
@@ -7,30 +7,27 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from operator import itemgetter
 
-#bakterite forward
-bac_prim=Seq("GTGYCAGCMGCCGCGGTAA")
-#bakterite reverse 
-bac_rev=Seq("AGTCAGCCAGCCGYCAATTYMTTTRAGTTT")
+#list to store primer tuples (name,Fprim,Rprim)
+primer_list=[]
+#populate from oligo file
+with open(str(sys.argv[2]), 'r') as f:
+ for line in f:
+  str_list=line.split('\t')
+  primer_list.append((str_list[0],Seq(str_list[1]),Seq(str_list[2].rstrip())))
 
-#arhede forward
-arc_prim=Seq("CAGYCGCCRCGGTAA")
-#arhede reverse
-arc_rev=Seq("GCYCCCCCGCCWATTC")
 
-#generate search strings to search + strand .. deg_replace replaces degenrate nucleotides
-#with appropriate nucleotide groups
-bacFsearch="(?b)("+deg_replace(str(bac_prim))+")"+"{e<=2}"
-bacRsearch="(?b)("+deg_replace(str(bac_rev.reverse_complement()))+")"+"{e<=2}"
+#list to store search string tuples
+#(name,plusFsearch,plusRsearch,minFSearch,minRsearch)
+prim_search_strings=[]
 
-arcFsearch="(?b)("+deg_replace(str(arc_prim))+")"+"{e<=2}"
-arcRsearch="(?b)("+deg_replace(str(arc_rev.reverse_complement()))+")"+"{e<=2}"
-
-#generate search strings for - strands
-bacFminsearch="(?b)("+deg_replace(str(bac_prim.reverse_complement()))+")"+"{e<=2}"
-bacRminsearch="(?b)("+deg_replace(str(bac_rev))+")"+"{e<=2}"
-
-arcFminsearch="(?b)("+deg_replace(str(arc_prim.reverse_complement()))+")"+"{e<=2}"
-arcRminsearch="(?b)("+deg_replace(str(arc_rev))+")"+"{e<=2}"
+#generate and store search string tuples
+for primtup in primer_list:
+ plusFsearch="(?b)("+deg_replace(str(primtup[1]))+")"+"{e<=2}"
+ plusRsearch="(?b)("+deg_replace(str(primtup[2].reverse_complement()))+")"+"{e<=2}"
+ minFsearch="(?b)("+deg_replace(str(primtup[1].reverse_complement()))+")"+"{e<=2}"
+ minRsearch="(?b)("+deg_replace(str(primtup[2]))+")"+"{e<=2}"
+ t=primtup[0],plusFsearch,plusRsearch,minFsearch,minRsearch
+ prim_search_strings.append(t)
 
 #create a dictionary  to store match scores
 valpairs={}
@@ -39,56 +36,32 @@ out = open(str(sys.argv[1])+".groups", 'w')
 
 #search sequences for matches
 record_iterator = SeqIO.parse(str(sys.argv[1]), "fasta")
+#for each sequence
 for seq_rec in record_iterator :
-
-#bac + 
- f=regex.search(bacFsearch,str(seq_rec.seq))
- if f is None:
-  match_score="NA"
- else:
-  r=regex.search(bacRsearch,str(seq_rec.seq))
-  if r is None:
+#match all posprimers
+ for search_tup in prim_search_strings:
+  f=regex.search(search_tup[1],str(seq_rec.seq))
+  if f is None:
    match_score="NA"
   else:
-   match_score=str(sum(f.fuzzy_counts)+sum(r.fuzzy_counts))
- valpairs["Bpos"]=match_score
-
-#bac -
- f=regex.search(bacFminsearch,str(seq_rec.seq))
- if f is None:
-  match_score="NA"
- else:
-  r=regex.search(bacRminsearch,str(seq_rec.seq))
-  if r is None:
+   r=regex.search(search_tup[2],str(seq_rec.seq))
+   if r is None:
+    match_score="NA"
+   else:
+    match_score=str(sum(f.fuzzy_counts)+sum(r.fuzzy_counts))
+  valpairs[str(search_tup[0])+"pos"]=match_score
+#match all minprimers
+ for search_tup in prim_search_strings:
+  f=regex.search(search_tup[3],str(seq_rec.seq))
+  if f is None:
    match_score="NA"
   else:
-   match_score=str(sum(f.fuzzy_counts)+sum(r.fuzzy_counts))
- valpairs["Bmin"]=match_score
-
-#arc+
-
- f=regex.search(arcFsearch,str(seq_rec.seq))
- if f is None:
-  match_score="NA"
- else:
-  r=regex.search(arcRsearch,str(seq_rec.seq))
-  if r is None:
-   match_score="NA"
-  else:
-   match_score=str(sum(f.fuzzy_counts)+sum(r.fuzzy_counts))
- valpairs["Apos"]=match_score
-
-#arc-
- f=regex.search(arcFminsearch,str(seq_rec.seq))
- if f is None:
-  match_score="NA"
- else:
-  r=regex.search(arcRminsearch,str(seq_rec.seq))
-  if r is None:
-   match_score="NA"
-  else:
-   match_score=str(sum(f.fuzzy_counts)+sum(r.fuzzy_counts))
- valpairs["Amin"]=match_score
+   r=regex.search(search_tup[4],str(seq_rec.seq))
+   if r is None:
+    match_score="NA"
+   else:
+    match_score=str(sum(f.fuzzy_counts)+sum(r.fuzzy_counts))
+  valpairs[str(search_tup[0])+"min"]=match_score
 #sort scores
  sortedlist =sorted(valpairs.items(), key=itemgetter(1))
 #write line to groupfile
@@ -99,10 +72,12 @@ for seq_rec in record_iterator :
    out.write(seq_rec.id+"\t"+"Ambig"+"\n")
   else:
    out.write(seq_rec.id+"\t"+sortedlist[0][0]+"\n")
- 
 
 #close group file handle
 out.close()
+
+
+
 
 
 
