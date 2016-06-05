46a1ee8e9c0b37a61fe99211dbeed717f248d6e7
jens-konrad preem <jenspreem@gmail.com>
2016-06-05 12:59:56 +0300

    main script to split fasta instead of writing a group file
    prim_sort.py now takes input fasta file and oligo file and
    splits fasta file into pieces named inputfile.primerpair.fa
    for each uniquely named primer pair in oligofile

--- prim_sort.py
+++ prim_sort.py
@@ -28,11 +28,8 @@ for primtup in primer_list:
  minRsearch="(?b)("+deg_replace(str(primtup[2]))+")"+"{e<=2}"
  t=primtup[0],plusFsearch,plusRsearch,minFsearch,minRsearch
  prim_search_strings.append(t)
-
 #create a dictionary  to store match scores
 valpairs={}
-#create a groupfile
-out = open(str(sys.argv[1])+".groups", 'w')
 
 #search sequences for matches
 record_iterator = SeqIO.parse(str(sys.argv[1]), "fasta")
@@ -49,7 +46,7 @@ for seq_rec in record_iterator :
     match_score="NA"
    else:
     match_score=str(sum(f.fuzzy_counts)+sum(r.fuzzy_counts))
-  valpairs[str(search_tup[0])+"pos"]=match_score
+  valpairs[str(search_tup[0])+"+"]=match_score  #for unique key denote + strand separately
 #match all minprimers
  for search_tup in prim_search_strings:
   f=regex.search(search_tup[3],str(seq_rec.seq))
@@ -61,20 +58,23 @@ for seq_rec in record_iterator :
     match_score="NA"
    else:
     match_score=str(sum(f.fuzzy_counts)+sum(r.fuzzy_counts))
-  valpairs[str(search_tup[0])+"min"]=match_score
+  valpairs[str(search_tup[0])+"-"]=match_score #for unique key denote - strand separately
+
 #sort scores
  sortedlist =sorted(valpairs.items(), key=itemgetter(1))
-#write line to groupfile
+#write line to one of the outputs
  if sortedlist[0][1] == "NA":
-  out.write(seq_rec.id+"\t"+"Nomatch"+"\n")
+  nm=open(str(sys.argv[1])+".NOMATCH.fa", 'a')
+  SeqIO.write(seq_rec,nm,"fasta")
  else:  
   if sortedlist[0][1]==sortedlist[1][1]:
-   out.write(seq_rec.id+"\t"+"Ambig"+"\n")
+   am=open(str(sys.argv[1])+".AMBIG.fa", 'a')
+   SeqIO.write(seq_rec,am,"fasta")
   else:
-   out.write(seq_rec.id+"\t"+sortedlist[0][0]+"\n")
+   sm=open(str(sys.argv[1])+"."+str(sortedlist[0][0])[:-1]+".fa", 'a') #[:-1]remove strand +/- marking
+   SeqIO.write(seq_rec,sm,"fasta")
 
-#close group file handle
-out.close()
+
 
 
 
